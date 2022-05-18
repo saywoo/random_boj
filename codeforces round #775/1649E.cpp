59f9a78b3fcce9cb668063bd81ed22563462d937
saywoo <saywoo1021@gmail.com>
2022-05-19 00:53:07 +0900

    2022/05/19

---
+++ codeforces round #775/1649E.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 998244353
+typedef long long int ll;
+ll n, m;
+vector<ll> v, w;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    cin >> n >> m;
+    v.resize(n); w.resize(m);
+
+    for (int i = 0; i < n; i++) cin >> v[i];
+    for (int i = 0; i < m; i++) cin >> w[i];
+
+    
+
+
+    return 0;
+}
